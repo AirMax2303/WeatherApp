a95182cd970fc500e5fa1da0aaea624e65a0511e
LysenkoWork <lyss.work@gmail.com>
2022-11-17 20:51:41 +0300

    create something and fake list for HoursFragment.kt

--- app/src/main/java/adapters/WeatherModel.kt
+++ app/src/main/java/adapters/WeatherModel.kt
@@ -3,10 +3,10 @@ package adapters
 data class WeatherModel(
     val city: String,
     val time: String,
-    val conditiond: String,
+    val condition: String,
     val currentTemp: String,
     val maxTemp: String,
     val minTemp: String,
-    val umageUrl: String,
+    val imageUrl: String,
     val hours: String,
 )
--- app/src/main/java/com/example/weatherapp/fragments/HoursFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/HoursFragment.kt
@@ -1,20 +1,80 @@
 package com.example.weatherapp.fragments
 
+import adapters.WeatherAdapter
+import adapters.WeatherModel
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root //inflater.inflate(R.layout.fragment_hours, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "",
+                "12:00",
+                "Sunny",
+                "25 C",
+                "",
+                "",
+                "",
+                ""
+            ),
+            WeatherModel(
+                "",
+                "15:00",
+                "Sunny",
+                "15 C",
+                "",
+                "",
+                "",
+                ""
+            ),
+            WeatherModel(
+                "",
+                "18:00",
+                "Sunny",
+                "25 C",
+                "",
+                "",
+                "",
+                ""
+            ),
+            WeatherModel(
+                "",
+                "19:00",
+                "Sunny",
+                "25 C",
+                "",
+                "",
+                "",
+                ""
+            ),
+        )
+        adapter.submitList(list)
     }
 
     companion object {
