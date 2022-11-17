8f8bc434ad17a4ccc56021202e875bdf6ad7bef3
LysenkoWork <lyss.work@gmail.com>
2022-11-17 21:31:26 +0300

    add test api in log (MainFragment.kt)

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.fragments
 import adapters.VpAdapter
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +12,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.RequestFuture
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+const val apiKey = "d1f03197da864cd095c85457221711"
 
 class MainFragment : Fragment() {
     private val fList = listOf(
@@ -39,6 +45,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("Krasnodar")
     }
 
     private fun init() = with(binding){
@@ -62,6 +69,29 @@ class MainFragment : Fragment() {
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
             // ToDo: если пользователь не дал разрешение, уведомить его об этом и о последствиях
         }
+    }
+
+    private fun requestWeatherData(city: String){
+
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                apiKey +
+                "&q=" +
+                city +
+                "&days=" +
+                "7" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result -> Log.d("MyLog", "Result: $result")
+            },
+            {
+                error -> Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
